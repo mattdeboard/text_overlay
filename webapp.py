abdbfa0ad49f5ff21e1f096636e90cf4df941620
Matt DeBoard <matt.deboard@gmail.com>
2010-11-09 15:16:02 -0500

    Major updates

--- webapp.py
+++ webapp.py
@@ -1,25 +1,28 @@
 import web
 from web import form
-from ragedraw import RageImage
+from ragedraw import *
 
-urls = ('/face', 'rageface')
-render = web.template.render('templates/')
+urls = ('/', 'rageface')
+render = web.template.render('C:\\python27\\text2rageface\\templates\\')
+imgname = "C:\\Documents and Settings\\mdeboard\\Desktop\\ragetext.jpg"
 
 app = web.application(urls, globals())
 
 input_form = form.Form(
-    form.Textbox('Enter text:',
+    form.Textbox('input',
+                 form.Validator("Text must be 12 characters or less", lambda x: len(x) < 13),
+                 pre="Enter text:",
                  maxlength='12'
-                 form.Validator("Text must be 12 characters or less", lambda x: len(x) < 13)
-                 )
-    form.Button('Submit')
+                 ),
     )
 
 class rageface:
 
     def GET(self):
-        rageimg = RageImage.returnbaseimage()
-        return render.rageface(rageimg)
+        form = input_form()
+        # make sure you create a copy of the form by calling it (line above)
+        # Otherwise changes will appear globally
+        return render.formtest(form)
 
     def POST(self):
         imgname = "C:\\Documents and Settings\\mdeboard\\Desktop\\ragetext.jpg"
@@ -27,7 +30,7 @@ class rageface:
         if not form.validates():
             return render.formtest(form)
         else:
-            RageImage.createimage(form['Enter text:'].value, imgname)
+            createimage(form['input'].value, imgname)
             return render.rageface(imgname)
 
 
