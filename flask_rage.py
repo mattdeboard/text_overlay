7bfe629361ddc0f7179455dfd2d0ed2ae774819d
mattdeboard <matt.deboard@gmail.com>
2011-01-24 23:08:46 -0500

    Total overhaul of core, basically.

--- flask_rage.py
+++ flask_rage.py
@@ -1,52 +1,57 @@
-##To Do:
-##    Create "Copy URL to Clipboard" device
-##    Work in Apache2/FastCGI image-serving
-##    
-##     
-##   
-##
-##
-##
-##
-##
-##
-##
-
 import Image, ImageDraw, ImageFont
-import sys
-import random
+import random, base64
+import sys, os
 from flask import Flask, render_template, request
 from os import remove
-##from flaskext.wtf import Form, TextField, validators
-from wtforms import Form, TextField, validators
+from flaskext.wtf import Form, TextField, validators, Required
+from imgur_upload import imgapi
 
 app = Flask(__name__)
+app.secret_key = 'b\xcaf\xde\xc3\xc6\xdc\x03\xf0ls\xd6\x08\xe7\x9a2\x02j\xdf\xa7n\xe5\xf4\xdd'
 
 class ImageForm(Form):
-    ragetext = TextField('Enter Rage Text:', [validators.Length(min=1, max=25)])
-    
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    form = ImageForm(request.form)
-    if request.method == 'GET':
-        return render_template('index.html', form=form)
-    else:
-        the_image=createimage(form.ragetext.data)
-        return render_template('ragetext.html', img=the_image)
+    ragetext = TextField("Enter rage text:", [validators.required()])
+
+@app.route('/', methods=("GET", "POST"))
+def rage():
+    try:
+        form = ImageForm()
+        form.ragetext.validate(form=form)
+        if form.validate_on_submit():
+            the_image, savefile = createimage(form.ragetext.data)
+            imgpath = imgur_up(the_image)
+            remove(savefile)
+            return render_template('rage.html', form=form, img=imgpath)
+        else:
+            return render_template('rage.html', form=form)
+    except Exception:
+        import traceback
+        print >> sys.stderr, ''.join(traceback.format_exception(*sys.exc_info()))
 
 def createimage(text):
-    image = Image.open("static/rageface.jpg")
+    image = Image.open("/a/mattdeboard.net/root/rage/static/rageface.jpg")
     draw = ImageDraw.Draw(image)
     if len(text) <= 7:
         fontsize = 72
-    else:        
+    else:
         fontsize = 48
-##    font = ImageFont.truetype("ARIALBD.ttf", fontsize, encoding="unic")
-    font = ImageFont.truetype("static/OldSansBlack.ttf", fontsize, encoding="unic")
+    font = ImageFont.truetype("/a/mattdeboard.net/root/rage/static/FreeSansBold.ttf", fontsize, encoding="unic")
     draw.text((10, 10), text, font=font, fill="red")
-    newname = 'images/%s.jpg' % random.randint(0,1000000000) 
-    image.save(newname)
-    return newname  
+    fname = '%s.jpeg' % random.randint(0,1000000000)
+    the_image = "images/%s" % fname
+    savefile = '/a/mattdeboard.net/root/rage/images/%s' % fname
+    image.save(savefile)
+    return the_image, savefile
+
+def imgur_up(img):
+    f = open('/a/mattdeboard.net/root/rage/%s' % img)
+    imgurl = imgapi.upload(f.read())
+    print >> sys.stderr, imgurl
+    try:
+        return imgurl[u'rsp'][u'image'][u'original_image']
+    except KeyError:
+        return img
+
 
 if __name__ == "__main__":
     app.run(debug=True)
